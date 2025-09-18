87e29941916e6f84aa11ac03da34dc5b766f3872
mellow012 <73447702+mellow012@users.noreply.github.com>
2025-09-18 16:07:16 +0200

    Update page.tsx

--- src/app/notifications/page.tsx
+++ src/app/notifications/page.tsx
@@ -1,3 +1,4 @@
+'use client';
 import React, { useEffect, useState } from 'react';
 import { db } from '@/lib/firebaseConfig';
 import { collection, query, where, orderBy, onSnapshot, updateDoc, doc } from 'firebase/firestore';
